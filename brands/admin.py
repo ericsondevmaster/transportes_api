a3fe9ca0bc152bb5c13c7befc5896a6f5b9b0265
ericsondevmaster <ericson.devmaster@gmail.com>
2024-04-23 13:04:36 -0300

    Configuring makemigrations and admin for brands

--- brands/admin.py
+++ brands/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from brands.models import Brand
 
-# Register your models here.
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+    def save_model(self, request, obj, form, change):
+        obj.name = obj.name.upper()
+        super().save_model(request, obj, form, change)
